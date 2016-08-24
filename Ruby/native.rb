fca1e5167e798c02e059237f3ba81e1fb440c157
ken210 <ken.rosaka@gmail.com>
2016-08-24 13:58:18 -0300

    chore(ruby): expand data object

--- Ruby/native.rb
+++ Ruby/native.rb
@@ -1,15 +1,22 @@
 require 'uri'
 require 'net/http'
+require 'json'
 
-url = URI("https://app.binds.co/api/seeds")
+url = URI('https://app.binds.co/api/seeds')
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 request = Net::HTTP::Post.new(url)
-request["content-type"] = 'application/json; charset=UTF-8'
-request.body = "{\"collector\": \"56a351796b80339c09ff1b76\", \"from\": {\"name\": \"Charlie Parker\", \"email\": \"hello@binds.co\"}}"
+request['content-type'] = 'application/json; charset=UTF-8'
+request.body = {
+  collector: '56a351796b80339c09ff1b76',
+  from: {
+    name: 'Charlie Parker',
+    email: 'hello@binds.co'
+  }
+}.to_json
 
 response = http.request(request)
 puts response.read_body
